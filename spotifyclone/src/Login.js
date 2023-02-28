cfe923f284e8ab6746ee483385c437592bc1f683
Jayanth R <jayanthrnambiar@gmail.com>
2023-02-28 12:19:11 +0530

    stage1

---
+++ spotifyclone/src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import  "./Login.css"
+const Login = () => {
+  return (
+    
+    <div className='login'>
+        <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-attempts-clarify-lack-google-cast-support-13.png" alt="logo"/>
+        <a>LOGIN WITH SPOTIFY</a>
+        <h1>hi</h1>
+        <h1>hi</h1>
+    </div>
+  )
+}
+
+export default Login
